39758f9ba28b8375e3e6d11afd5e346825f47341
almaz <almazgimaev@gmail.com>
2023-11-30 15:20:37 +0100

    load credentials if dev mode

--- src/globals.py
+++ src/globals.py
@@ -3,18 +3,17 @@ import sys
 from pathlib import Path
 
 import supervisely as sly
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from supervisely.app.v1.app_service import AppService
 
 root_source_dir = str(Path(sys.argv[0]).parents[1])
 print(f"App source directory: {root_source_dir}")
 sys.path.append(root_source_dir)
 
-# only for convenient debug
-# debug_env_path = os.path.join(root_source_dir, "debug.env")
-# secret_debug_env_path = os.path.join(root_source_dir, "secret_debug.env")
-# load_dotenv(debug_env_path)
-# load_dotenv(secret_debug_env_path, override=True)
+if sly.is_development():
+    debug_env_path = os.path.join(root_source_dir, "debug.env")
+    load_dotenv(debug_env_path)
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
 
 my_app = AppService()
 api: sly.Api = my_app.public_api
